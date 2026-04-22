bd5cce9c1fbd01696a2515f9ccdf0306141fb888
Vitória Fernandes <114036092+vittfs@users.noreply.github.com>
2026-04-22 14:57:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Seleciona o elemento com a classe 'card'
+let card = document.querySelector('.card');
+
+// Seleciona o botão de login
+let loginButton = document.querySelector('.loginButton');
+
+// Seleciona o botão de cadastro
+let cadastroButton = document.querySelector('.cadastroButton');
+
+// Adiciona um evento de clique ao botão de login
+loginButton.onclick = () => {
+    // Remove a classe 'cadastroActive' do card
+    card.classList.remove('cadastroActive');
+    // Adiciona a classe 'loginActive' ao card
+    card.classList.add('loginActive');
+}
+
+// Adiciona um evento de clique ao botão de cadastro
+cadastroButton.onclick = () => {
+    // Remove a classe 'loginActive' do card
+    card.classList.remove('loginActive');
+    // Adiciona a classe 'cadastroActive' ao card
+    card.classList.add('cadastroActive');
+}
